b5c6f1604a0a9a5a659bd9b2ef05a594046913fa
ProrokWielki <ProrokWielki@o2.pl>
2025-07-20 17:53:55 +0200

    updated widget

--- Widget.hpp
+++ Widget.hpp
@@ -9,23 +9,32 @@
 #define APP_GUI_WIDGET_HPP_
 
 #include <cstdint>
+#include <vector>
+
+class Widget;
+
+struct WidgetAndPositions {
+    uint8_t x;
+    uint8_t y;
+    Widget * widget;
+};
 
 class Widget
 {
 public:
     Widget() = default;
 
-    uint8_t getWidth()
+    [[nodiscard]] uint8_t getWidth() const
     {
         return width_;
     }
 
-    uint8_t getHeight()
+    [[nodiscard]] uint8_t getHeight() const
     {
         return height_;
     }
 
-    uint8_t * get_pixel_map()
+    [[nodiscard]] const uint8_t * get_pixel_map() const
     {
         return pixel_map_;
     }
@@ -34,7 +43,7 @@ public:
     {
     }
 
-    virtual bool is_self_updatable()
+    [[nodiscard]] virtual bool is_self_updatable() const
     {
         return false;
     }
@@ -43,12 +52,12 @@ public:
     {
     }
 
-    void setPixelMap(uint8_t * width)
+    void setPixelMap(const uint8_t * pixel_map)
     {
-        pixel_map_ = width;
+        pixel_map_ = pixel_map;
     }
 
-    bool is_visible()
+    [[nodiscard]] bool is_visible() const
     {
         return is_visible_;
     }
@@ -63,6 +72,22 @@ public:
         is_visible_ = true;
     }
 
+    void add_child(Widget * child, uint8_t x = 0, uint8_t y = 0)
+    {
+        children_.push_back({x, y, child});
+    }
+
+    void clear_children()
+    {
+        children_.clear();
+    }
+
+    [[nodiscard]] const std::vector<WidgetAndPositions> & get_children() const
+    {
+        return children_;
+    }
+
+
 protected:
     void setWidth(uint8_t width)
     {
@@ -76,9 +101,13 @@ protected:
     uint8_t width_{};
     uint8_t height_{};
 
-    uint8_t * pixel_map_{};
+    const uint8_t * pixel_map_{};
 
     bool is_visible_{true};
+    
+private:
+    std::vector<WidgetAndPositions> children_{};
+
 };
 
 #endif /* APP_GUI_WIDGET_HPP_ */
